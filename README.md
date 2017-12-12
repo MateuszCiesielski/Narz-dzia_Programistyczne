58e0caa2e770fd8d10165ee637a4fb3b510f7327
Fiefior <amatusiak010@gmail.com>
2017-12-12 13:05:32 +0100

    CSV table

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Narzedzia_Programistyczne
+# https://danepubliczne.gov.pl/group/biznes_gospodarka?tags=sprawozdanie+misi%C4%99czne&organization=ministerstwo_finansow
--- app.R
+++ app.R
@@ -1,3 +1,2 @@
 # Create Shiny app ----
 shinyApp(ui, server)
-#https://danepubliczne.gov.pl/group/biznes_gospodarka?tags=sprawozdanie+misi%C4%99czne&organization=ministerstwo_finansow
