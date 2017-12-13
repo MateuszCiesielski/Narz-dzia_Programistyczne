7ad81ec4c102ff22bd120fe87663ad7835d679ad
Artur Matusiak <amatusiak010@gmail.com>
2017-12-13 23:33:37 +0100

    Nie dziekujcie

--- server.R
+++ server.R
@@ -1,9 +1,6 @@
 # Rely on the 'WorldPhones' dataset in the datasets
 # package (which generally comes preloaded).
 library(datasets)
-
-
-
 # Define a server for the Shiny app
 function(input, output) {
   
@@ -12,25 +9,28 @@ function(input, output) {
   print(ncol(data))
   print(nrow(data))
   ########## Sortuje calego data frame'a i wyciagam top 5 bezrobotnych zawodow, much wow###############
-  df <- head(data[order(data$Bezrobotni.dlugotrwale.na.koniec.okresu, decreasing= T),], n = 5)
-  # df <- head(sort(data$Bezrobotni.dlugotrwale.na.koniec.okresu,decreasing=TRUE),n=5)
-  print(df)
+
  # str(data)
-  
+  #####Tutaj ustawiaj zmienne dla wykresu
+  ######W przyszlosci niech server dostaje wszystkie potrzebne rzeczy w inpucie
   ####################################################################
-  WartosciX <- df$Bezrobotni.dlugotrwale.na.koniec.okresu
-  WartosciY <- df$Elementarne.grupy.zawodow
+ # ss = input$inp
+ # WartosciX <- df$Bezrobotni.dlugotrwale.na.koniec.okresu
   NazwaY<-"Bezrobotni dlugotrwale na koniec okresu"
   NazwaX<-"Ilosc"
   #####################################################################
 
   # Fill in the spot we created for a plot
-  output$phonePlot <- renderPlot({
-
+  output$PracaPlot <- renderPlot({
+    df <- head(data[order(data[input$Kolumna], decreasing= T),], n = input$IloscZawodow)
+    WartosciY <- df$Elementarne.grupy.zawodow
+    print(df)
     
     # Plot the bar chart.
-    barplot(WartosciX,names.arg = WartosciY,xlab = NazwaX,ylab = NazwaY,col = "blue",
-            main = "Na Na Na Bezrobocie",border = "red")
+    barplot(df[,input$Kolumna]#WartosciX
+            ,names.arg = WartosciY,xlab = NazwaX,ylab = NazwaY,col = "blue",
+            main = input$inp#"Na Na Na Bezrobocie"
+            ,border = "red")
     
   })
 }
--- ui.R
+++ ui.R
@@ -1,7 +1,8 @@
 # Rely on the 'WorldPhones' dataset in the datasets
 # package (which generally comes preloaded).
 library(datasets)
-
+data <-read.csv(file="bezrobotni.csv",header=TRUE,sep=";")
+NazwyKolumn=colnames(data)
 # Use a fluid Bootstrap layout
 fluidPage(    
   
@@ -13,15 +14,25 @@ fluidPage(
     
     # Define the sidebar with one input
     sidebarPanel(
-    # selectInput("region", "Region:", 
-     #             choices=colnames(WorldPhones)),
-      hr(),
-      helpText("Data from AT&T (1961) The World's Telephones.")
-    ),
+      
+      ## Pierwszy argument to nazwa ""zmiennej"" do ktorej sie odwolamy w serverze
+      ## przez input$Kolumna
+      ##Druga wyswietla sie na ekranie, po prostu nazwa
+      ##Inputu nie mozesz pobierac tylko w funkcji "renderplot" 
+      ##lub innej funkcji reagujacej dynamicznie na zmiany(???)
+      selectInput("Kolumna","Inp:",
+                  choices=NazwyKolumn),
+      hr(), ####co to kuhwa to hr
+      helpText("Wybierz zawody dla ktorych chcesz wyswietlic stan"),
+      #Selektuje ile wyswietlic top zawodow
+      ##Potem beda tryby, top N bezrobotnych lub wybrane zawody
+      sliderInput("IloscZawodow","Wyswietl top N:",
+                  min = 0, max = 10,
+                  value=5)),
     
     # Create a spot for the barplot
     mainPanel(
-      plotOutput("phonePlot")  
+      plotOutput("PracaPlot")  
     )
     
   )
