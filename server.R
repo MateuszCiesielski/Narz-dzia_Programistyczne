2fd98767aef0d70652993a3fb3bd192b109f28f6
paulamaciejewska <paula.mac@o2.pl>
2018-01-23 03:42:45 +0100

    some changes

--- server.R
+++ server.R
@@ -1,38 +1,37 @@
 function(input, output) {
   
-  
-  output$WybierajkaZawodow<- renderUI({
-    if(!is.null(input$file1))
+  output$DataColumn <- renderUI(
     {
-      file<-input$file1
+      if(!is.null(input$UploadedFile))
+      {
+        file<-input$UploadedFile
+        data<-read.csv(file$datapath,header=TRUE,sep=";",encoding="UTF-8",dec=",")
+        NazwyKolumn<-colnames(subset(data,select = -c(1,2,18,19)))
+        selectInput("Column","Wybierz kategorię:",
+                    choices=ColumnNames)
+      }
+    })
+  
+  output$Profession <- renderUI({
+    if(!is.null(input$UploadedFile))
+    {
+      file<-input$UploadedFile
       data <- read.csv(file$datapath,header=TRUE,sep=";",encoding="UTF-8",dec=",")
-
+      
       selectizeInput(
-      'WybraneZawody', '2. Multi-select',
+      'WybraneZawody', 'Wybierz zawody dla ktorych chcesz wyswietlic stan:',
       choices=data$Elementarne.grupy.zawodów,
       multiple = TRUE
       )
     }
   })
   
-  output$WybierajkaKolumn<- renderUI(
-  {
-   if(!is.null(input$file1))
-   {
-     file<-input$file1
-    data<-read.csv(file$datapath,header=TRUE,sep=";",encoding="UTF-8",dec=",")
-    NazwyKolumn<-colnames(subset(data,select = -c(1,2,18,19)))
-    selectInput("Kolumna","Inp:",
-                choices=NazwyKolumn)
-   }
-  })
-  
   output$PracaPlot <- renderPlot({
     
-    if(!is.null(input$file1))
+    if(!is.null(input$UploadedFile))
     {
       
-    file <- input$file1
+    file <- input$UploadedFile
     data <-read.csv(file$datapath,header=TRUE,sep=";",encoding="UTF-8",dec=",")
     data[,-c(1,18,19)] #wywalanie kolumn
     
--- ui.R
+++ ui.R
@@ -10,15 +10,17 @@ fluidPage(
   sidebarLayout(      
     # Define the sidebar with one input
     sidebarPanel(
-      fileInput("f","Wybierz plik CSV"),
+      fileInput("UploadedFile","Wybierz plik CSV"),
       hr(),
-      ##test
       ## Pierwszy argument to nazwa ""zmiennej"" do ktorej sie odwolamy w serverze
       ## przez input$Kolumna
       ##Druga wyswietla sie na ekranie, po prostu nazwa
       ##Inputu nie mozesz pobierac tylko w funkcji "renderplot" 
       ##lub innej funkcji reagujacej dynamicznie na zmiany(???)
-      uiOutput("WybierajkaKolumn"),
+      uiOutput("DataColumn"),
+      helpText("Wybierz zawody dla ktorych chcesz wyswietlic stan:"),
+      uiOutput("ProfessionLabel"),
+      uiOutput("Profession"),
       hr(),##to tylko linia pozioma miedzy inputami
       #Selektuje ile wyswietlic top zawodow
       helpText("Czy wyswietlic top N zawodow w danej kategorii?"),
@@ -28,13 +30,7 @@ fluidPage(
         sliderInput("IloscZawodow","Wyswietl top N:",
                     min = 0, max = 10,
                     value=0)
-      ),
-      helpText("Wybierz zawody dla ktorych chcesz wyswietlic stan"),
-  #    selectizeInput(
-  #      'WybraneZawody', '2. Multi-select', choices = output$Zawody, multiple = TRUE
-  #    )
-      uiOutput("WybierajkaZawodow")
-      
+      )
     ),
     # Create a spot for the barplot
     mainPanel(
